4b579596fd907d9ba5ba995fea378642ea5841c2
amosbastian <amosbastian@gmail.com>
2018-08-12 23:47:28 +0100

    update: add additional column

--- new_sheet.py
+++ new_sheet.py
@@ -1,25 +1,12 @@
 from datetime import date, timedelta
-from oauth2client.service_account import ServiceAccountCredentials
+import constants
 import gspread
-import os
-
-# Get path of current folder
-DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-
-# Everything outside because I can't be bothered doing this properly
-scope = ["https://spreadsheets.google.com/feeds",
-         "https://www.googleapis.com/auth/drive"]
-credentials = ServiceAccountCredentials.from_json_keyfile_name(
-    f"{DIR_PATH}/client_secret.json", scope)
-client = gspread.authorize(credentials)
-sheet = client.open("Utopian Reviews")
 
 
 def main():
     # Date stuff
     today = date.today()
-    offset = (today.weekday() - 3) % 7
-    this_week = today - timedelta(days=offset)
+    this_week = today - timedelta(days=constants.OFFSET)
     last_week = this_week - timedelta(days=7)
     next_week = this_week + timedelta(days=7)
     title_last = f"Unreviewed - {last_week:%b %-d} - {this_week:%b %-d}"
@@ -27,12 +14,12 @@ def main():
     title_reviewed = f"Reviewed - {this_week:%b %-d} - {next_week:%b %-d}"
 
     # Get sheet and some variables
-    reviews = sheet.worksheet(title_last)
+    reviews = constants.SHEET.worksheet(title_last)
     header = reviews.row_values(1)
     reviews.update_title(title_unreviewed)
 
-    worksheet = sheet.add_worksheet(
-        title=title_reviewed, rows="999", cols="11")
+    worksheet = constants.SHEET.add_worksheet(
+        title=title_reviewed, rows="999", cols="12")
     worksheet.append_row(header)
 
 if __name__ == '__main__':
