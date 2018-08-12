a3597cb6dec0870a10ed6c0652bb658ea539fc19
amosbastian <amosbastian@gmail.com>
2018-08-12 23:48:54 +0100

    update: use Contribution class and constants, add additional column

--- reviewed_sheet.py
+++ reviewed_sheet.py
@@ -1,64 +1,7 @@
 from beem.comment import Comment
-from datetime import date, timedelta
-from oauth2client.service_account import ServiceAccountCredentials
-import gspread
-import logging
-import pprint
-import time
+from contribution import Contribution
+import constants
 import os
-
-# Get path of current folder
-DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-
-# Minimum score
-MINIMUM_SCORE = 10
-
-# Logging
-logger = logging.getLogger("utopian-io")
-logger.setLevel(logging.INFO)
-fh = logging.FileHandler(f"{DIR_PATH}/reviewed.log")
-fh.setLevel(logging.DEBUG)
-formatter = logging.Formatter(
-    "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-fh.setFormatter(formatter)
-logger.addHandler(fh)
-
-# Everything outside because I can't be bothered doing this properly
-scope = ["https://spreadsheets.google.com/feeds",
-         "https://www.googleapis.com/auth/drive"]
-credentials = ServiceAccountCredentials.from_json_keyfile_name(
-    f"{DIR_PATH}/client_secret.json", scope)
-client = gspread.authorize(credentials)
-sheet = client.open("Utopian Reviews")
-
-today = date.today()
-offset = (today.weekday() - 3) % 7
-this_week = today - timedelta(days=offset)
-next_week = this_week + timedelta(days=7)
-title_unreviewed = f"Unreviewed - {this_week:%b %-d} - {next_week:%b %-d}"
-title_reviewed = f"Reviewed - {this_week:%b %-d} - {next_week:%b %-d}"
-
-unreviewed = sheet.worksheet(title_unreviewed)
-reviewed = sheet.worksheet(title_reviewed)
-
-MAX_VOTE = {
-    "ideas": 20.0,
-    "development": 55.0,
-    "bug-hunting": 13.0,
-    "translations": 35.0,
-    "graphics": 40.0,
-    "analysis": 45.0,
-    "social": 30.0,
-    "documentation": 30.0,
-    "tutorials": 30.0,
-    "video-tutorials": 35.0,
-    "copywriting": 30.0,
-    "blog": 30.0,
-}
-MAX_TASK_REQUEST = 6.0
-
-# Exponential vote
-EXP_POWER = 2.1
 
 
 def exponential_vote(score, category):
@@ -68,46 +11,52 @@ def exponential_vote(score, category):
     status = ""
 
     try:
-        max_vote = MAX_VOTE[category]
+        max_vote = constants.MAX_VOTE[category]
     except:
-        max_vote = MAX_TASK_REQUEST
+        max_vote = constants.MAX_TASK_REQUEST
 
-    if score < MINIMUM_SCORE:
-        vote_pct = 0.0
+    if score < constants.MINIMUM_SCORE:
+        weight = 0.0
     else:
         status = "Pending"
-        vote_pct = pow(
+        power = constants.EXP_POWER
+        weight = pow(
             score / 100.0,
-            EXP_POWER - (score / 100.0 * (EXP_POWER - 1.0))) * max_vote
+            power - (score / 100.0 * (power - 1.0))) * max_vote
 
-    return status, f"{vote_pct:.2f}"
+    return status, f"{weight:.2f}"
 
 
-def vote(url, vote_pct):
-    vote_pct = float(vote_pct) / max(MAX_VOTE.values()) * 100.0
+def vote(url, weight):
+    weight = float(weight) / max(constants.MAX_VOTE.values()) * 100.0
     contribution = Comment(url)
-    contribution.vote(vote_pct, "amosbastian")
+    contribution.vote(weight, "amosbastian")
 
 
 def main():
-    time.sleep(1)
-    result = unreviewed.get_all_values()
+    result = constants.UNREVIEWED.get_all_values()
     for row in result[1:]:
-        moderator = row[0]
-        date = row[1]
-        score = row[5]
+        contribution = Contribution(row)
+        moderator = contribution.moderator
+        date = contribution.review_date
+        score = contribution.score
+
         if moderator != "" and date != "" and score != "":
             # Calculate voting %
-            category = row[4].strip()
-            row[-2], row[-1] = exponential_vote(float(score), category)
+            category = contribution.category.strip()
+            contribution.vote_status, contribution.weight = exponential_vote(
+                float(score), category)
+            contribution.review_status = "Pending"
 
-            logger.info(f"Moving {row[2]} to reviewed sheet with voting % "
-                        f"{row[-1]} and score {score}")
-            unreviewed.delete_row(result.index(row) + 1)
-            reviewed.append_row(row)
+            constants.LOGGER.info(
+                f"Moving {contribution.url} to reviewed sheet with voting % "
+                f"{contribution.weight} and score {score}")
 
-            if float(score) > MINIMUM_SCORE:
-                vote(row[2], row[-1])
+            constants.UNREVIEWED.delete_row(result.index(row) + 1)
+            constants.REVIEWED.append_row(list(contribution.__dict__.values()))
+
+            if float(score) > constants.MINIMUM_SCORE:
+                vote(contribution.url, contribution.weight)
             return
 
 if __name__ == '__main__':
--- spreadsheet.py
+++ spreadsheet.py
@@ -2,90 +2,21 @@ from beem.discussions import Query, Discussions_by_created
 from beem.comment import Comment
 from datetime import date, timedelta
 from oauth2client.service_account import ServiceAccountCredentials
-from pymongo import MongoClient
 from urllib.parse import urlparse
 
+import constants
 import gspread
 import json
 import logging
 import os
 
-# Get path of current folder
-DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-
-# Logging
-logger = logging.getLogger("utopian-io")
-logger.setLevel(logging.INFO)
-fh = logging.FileHandler(f"{DIR_PATH}/spreadsheet.log")
-fh.setLevel(logging.DEBUG)
-formatter = logging.Formatter(
-    "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-fh.setFormatter(formatter)
-logger.addHandler(fh)
-
-# Spreadsheet
-scope = ["https://spreadsheets.google.com/feeds",
-         "https://www.googleapis.com/auth/drive"]
-credentials = ServiceAccountCredentials.from_json_keyfile_name(
-    f"{DIR_PATH}/client_secret.json", scope)
-client = gspread.authorize(credentials)
-sheet = client.open("Utopian Reviews")
-
-# Dates
-today = date.today()
-offset = (today.weekday() - 3) % 7
-this_week = today - timedelta(days=offset)
-next_week = this_week + timedelta(days=7)
-last_week = this_week - timedelta(days=7)
-
-# Use title to select worksheet
-title_unreviewed = f"Unreviewed - {this_week:%b %-d} - {next_week:%b %-d}"
-title_reviewed = f"Reviewed - {this_week:%b %-d} - {next_week:%b %-d}"
-title_last = f"Reviewed - {last_week:%b %-d} - {this_week:%b %-d}"
-
-# Get all relevant worksheets
-unreviewed = sheet.worksheet(title_unreviewed)
-reviewed = sheet.worksheet(title_reviewed)
-last = sheet.worksheet(title_last)
-utopian_fest = sheet.worksheet("Utopian Fest")
-
-# Get all relevant URLs
-banned_sheet = sheet.worksheet("Banned users")
-banned_users = zip(banned_sheet.col_values(1), banned_sheet.col_values(4))
-
-# Get all translators
-translator_sheet = sheet.worksheet("Translators")
-UTOPIAN_TRANSLATORS = [translator.strip() for
-                       translator in translator_sheet.col_values(1)[1:]]
-
-# URL
-URL = "https://steemit.com/"
-
-# MongoDB
-CLIENT = MongoClient()
-DB = CLIENT.utopian
-
-# Points per category
-CATEGORIES = {
-    "ideas": 2.0,
-    "development": 4.25,
-    "graphics": 3.0,
-    "bug-hunting": 3.25,
-    "analysis": 3.25,
-    "social": 2.0,
-    "video-tutorials": 4.0,
-    "tutorials": 4.0,
-    "copywriting": 2.0,
-    "documentation": 2.25,
-    "blog": 2.25,
-    "translations": 4.0
-}
-
 
 def valid_category(tags):
-    """Returns True if category is valid, otherwise False"""
+    """
+    Returns True if category is valid, otherwise False
+    """
     for category in tags:
-        if "task" in category:
+        if "task-" in category:
             if "bug" in category:
                 return True, "task-bug-hunting"
             return True, category
@@ -117,7 +48,9 @@ def valid_category(tags):
 
 
 def get_repository(post):
-    """Returns the first repository found in the given post."""
+    """
+    Returns the first repository found in the given post.
+    """
     url = "github.com/"
     try:
         for link in post.json_metadata["links"]:
@@ -131,77 +64,45 @@ def get_repository(post):
 
 
 def moderator_points():
-    """Return a dict containing a moderator and their points."""
+    """
+    Return a dictionary containing a moderator and their points.
+    """
     moderators = {}
+    collection = constants.DB.moderators
 
-    # Include moderators who haven't reviewed anything yet
-    collection = DB.moderators
-    for moderator in collection.find():
-        moderators.setdefault(moderator["account"], {
-            "points": 0,
-            "reviewed": 0
-        })
-
-    # Zip moderator's name and category together
-    data = zip(reviewed.col_values(1), reviewed.col_values(5))
-    for moderator, category in data:
-        try:
-            moderator = moderator.lower().strip()
-        except Exception:
-            pass
-
-        if moderator == "banned" or moderator == "moderator":
-            continue
-
-        # Moderator not in database!
-        if moderator not in moderators.keys():
-            moderators[moderator] = {
-                "points": 0,
-                "reviewed": 0
-            }
-
-        try:
-            # Normal contribution
-            moderators[moderator]["points"] += CATEGORIES[category]
-        except:
-            # Task request
-            moderators[moderator]["points"] += 1.25
-
-        moderators[moderator]["reviewed"] += 1
-
-    # Check if moderator if community manager -> add 100 points
     community_managers = [
         moderator["account"] for moderator in
         collection.find({"supermoderator": True})]
 
-    for moderator, value in moderators.items():
+    utopian_fest = constants.UTOPIAN_FEST.col_values(1)
+
+    for moderator in set(community_managers + utopian_fest):
+        moderators.setdefault(moderator, 0)
         if moderator in community_managers:
-            value["points"] += 100.0
+            moderators[moderator] += 100.0
 
             # Check for BOSSPOEM or TECHSLUT
             if moderator == "espoem" or moderator == "techslut":
-                value["points"] = 400.0
-        else:
-            if value["reviewed"] >= 5:
-                value["points"] += 30.0
+                moderators[moderator] = 400.0
 
         # Utopian Fest bonus
-        if moderator in utopian_fest.col_values(1):
-            value["points"] += 50.0
-
-        moderators[moderator] = value["points"]
+        if moderator in utopian_fest:
+            moderators[moderator] += 50.0
 
     # Save dictionary as JSON with date of last Thursday
     with open(
-            f"/home/amos/utopian/utopian/static/{this_week}.json",
+            f"/home/amos/utopian/utopian/static/{constants.THIS_WEEK}.json",
             "w") as fp:
         json.dump(moderators, fp, indent=4)
 
 
 def get_urls():
-    return (unreviewed.col_values(3) +
-            reviewed.col_values(3) +
-            last.col_values(3))
+    """
+    Returns all the URLs of the currently relevant worksheets.
+    """
+    return (constants.UNREVIEWED.col_values(3) +
+            constants.REVIEWED.col_values(3) +
+            constants.LAST.col_values(3))
 
 
 def banned_comment(url):
@@ -209,11 +110,7 @@ def banned_comment(url):
     Comments on the given contribution letting them know that they are banned.
     """
     post = Comment(url)
-    comment = (
-        f"Hi, @{post.author}. Your account has been excluded from the Utopian "
-        "rewarding system and therefore this contribution post will not be "
-        "reviewed. You can contact us at https://support.utopian.io/ if you "
-        "have any further questions.")
+    comment = constants.COMMENT_BANNED.format(post.author)
     post.reply(comment, author="amosbastian")
 
 
@@ -225,40 +122,39 @@ def main():
     query = Query(limit=100, tag="utopian-io")
     result = get_urls()
     for post in Discussions_by_created(query):
-        steemit_url = f"{URL}{post.category}/{post.authorperm}"
+        steemit_url = (
+            f"{constants.STEEMIT_URL}{post.category}/{post.authorperm}")
         if steemit_url not in result:
 
             tags = post.json_metadata["tags"]
 
             # Checking if valid post
-            if (len(tags) < 2 or post["created"].date() < this_week):
+            if (len(tags) < 2 or post["created"].date() < constants.THIS_WEEK):
                 continue
             else:
                 is_valid, category = valid_category(tags)
                 if not is_valid:
-                    logger.error(f"{steemit_url} has tags: {tags} and was not "
-                                 "added")
                     continue
                 elif (category == "translations" and
-                      post.author not in UTOPIAN_TRANSLATORS):
-                    logger.error(f"{steemit_url} not made by accepted "
-                                 f"translator!")
+                      post.author not in constants.UTOPIAN_TRANSLATORS):
+                    constants.LOGGER.error(
+                        f"{steemit_url} not made by accepted translator!")
                     continue
             repository = get_repository(post)
 
             # If user banned, set moderator as BANNED and score to 0
-            if (post.author, "Yes") not in banned_users:
+            if (post.author, "Yes") not in constants.BANNED_USERS:
                 row = ["", "", steemit_url, repository, category]
             else:
                 row = ["BANNED", str(today), steemit_url, repository, category,
-                       "0", "", "", "", 0]
-                logger.info(f"Commenting on {steemit_url} letting them know "
-                            "that they are banned.")
+                       "0", "", "", "", "", 0]
+                constants.LOGGER.info(
+                    f"Commenting on {steemit_url} - BANNED.")
                 banned_comment(steemit_url)
-            unreviewed.append_row(row)
+            constants.UNREVIEWED.append_row(row)
             result = get_urls()
-            logger.info(f"{steemit_url} has tags: {tags} and was added to the "
-                        "spreadsheet.")
+            constants.LOGGER.info(
+                f"{steemit_url} has tags: {tags} and was added.")
 
     moderator_points()
 
@@ -267,4 +163,4 @@ if __name__ == '__main__':
     try:
         main()
     except Exception as error:
-        logger.error(error)
+        constants.LOGGER.error(error)
